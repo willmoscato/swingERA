1f9c6974fd5f79c08f38e0ed9c1a0e3f43411ace
wmoscato <wmoscato@macalester.edu>
2022-07-08 09:30:43 +0200

    shiny

---
+++ ERAprediction/app.R
@@ -0,0 +1,118 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(dplyr)
+library(DALEX)
+library(DALEXtra)
+library(themis)
+set.seed(494)
+ERAswing <- read_csv("era_swing2.csv")
+ERAswing <- ERAswing %>% 
+  select(-...16)
+erasplit <- initial_split(ERAswing, strata = "p_era")
+era_training <- training(erasplit)
+era_testing <- testing(erasplit)
+
+era_training <- era_training %>% 
+  select(-first_name, -last_name, -player_id, -year)
+model <- readRDS("eralasso.rds")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  titlePanel("ERA Prediction Based on Swing and Pitch Location Data"),
+
+  sidebarLayout(
+    sidebarPanel(
+    # Application title
+    
+
+   
+    sliderInput(inputId = "z_swing_percent", 
+                label = "Zone Swing Percentage",
+                min = 0, 
+                max = 100, 
+                value = 50),
+    sliderInput(inputId = "z_swing_miss_percent", 
+                label = "Zone Swing and Miss Percentage",
+                min = 0, 
+                max = 100, 
+                value = 50),
+    sliderInput(inputId = "oz_swing_percent", 
+                label = "Out of Zone Swing Percentage",
+                min = 0, 
+                max = 100, 
+                value = 50),
+    sliderInput(inputId = "oz_swing_miss_percent", 
+                label = "Out of Zone Swing and Miss Percentage",
+                min = 0, 
+                max = 100, 
+                value = 50),
+    sliderInput(inputId = "meatball_swing_percent", 
+                label = "Meatball Swing Percentage",
+                min = 0, 
+                max = 100, 
+                value = 50),
+    sliderInput(inputId = "meatball_percent", 
+                label = "Meatball Percentage",
+                min = 0, 
+                max = 100, 
+                value = 50),
+    sliderInput(inputId = "iz_contact_percent", 
+                label = "In Zone Contact Percentage",
+                min = 0, 
+                max = 100, 
+                value = 50),
+    sliderInput(inputId = "in_zone_percent", 
+                label = "In ZonePercentage",
+                min = 0, 
+                max = 100, 
+                value = 50),
+    sliderInput(inputId = "edge_percent", 
+                label = "Edge Percentage",
+                min = 0, 
+                max = 100, 
+                value = 50),
+    sliderInput(inputId = "swing_percent", 
+                label = "Swing Percentage",
+                min = 0, 
+                max = 100, 
+                value = 50),
+    submitButton(text = "Create my plot!")),
+  mainPanel(
+    plotOutput(outputId = "predictor")
+)))
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$predictor <- renderPlot({
+      predict_test <- data.frame(input$z_swing_percent, input$z_swing_miss_percent, input$oz_swing_percent, input$oz_swing_miss_percent, input$meatball_swing_percent, input$meatball_percent, input$iz_contact_percent, input$in_zone_percent, input$edge_percent, input$swing_percent)
+      names(predict_test) <- c("z_swing_percent", "z_swing_miss_percent", "oz_swing_percent", "oz_swing_miss_percent", "meatball_swing_percent", "meatball_percent", "iz_contact_percent", "in_zone_percent", "edge_percent", "swing_percent")
+      
+      era_lasso_explain <- 
+        explain_tidymodels(
+          model = model,
+          data = era_testing %>% select(-p_era), 
+          y = as.numeric(era_training$p_era),
+          label = "lasso"
+        )
+      
+      test <- predict_parts(explainer = era_lasso_explain,
+                            new_observation = predict_test,
+                            type = "break_down") #default
+      test_pp <- plot(test, title = "Predicted ERA") + theme(plot.title = element_text(hjust = .5, size = 15, color = "black", face = "bold"))
+      
+      test_pp
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
